b973bf5bde201691a55602de84ed6e5496146ef4
rpatetti <patetti.rafael@gmail.com>
2024-10-02 18:33:14 -0300

    initial commit

---
+++ src/exercicio_modulo_8/Calculo_medio.java
@@ -0,0 +1,17 @@
+package exercicio_modulo_8;
+
+public class Calculo_medio {
+
+	public static void main(String args[]) {
+		
+		double num1 = 23;
+		double num2 = 18;
+		double num3 = 16;
+		double num4 = 5;
+		
+		double num5 = (num1 + num2 + num3 + num4) / 4;
+		
+		System.out.println("A media é: " + num5);
+				
+	}
+}
